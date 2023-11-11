cd8fb406785e78974af8e9a33e8ece592bb5a69c
racoi <87438305+racoi@users.noreply.github.com>
2023-11-11 17:25:45 +0900

    [D2] Title: 파리 퇴치, Time: 147 ms, Memory: 21,916 KB -BaekjoonHub

---
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -0,0 +1,74 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			int N = sc.nextInt();
+            int M = sc.nextInt();
+            int arr[][] = new int[N][N];
+            
+            for(int i=0; i<N; i++){
+                for(int j=0; j<N; j++){
+                    arr[i][j] = sc.nextInt();
+                }
+            }
+            
+            int max = 0;
+            
+            for(int a=0; a<N-M+1; a++){
+                for(int b=0; b<N-M+1; b++){
+                    int sum = 0;
+                    for(int c=0; c<M; c++){
+                        for(int d=0; d<M; d++){
+                            sum += arr[a+c][b+d];
+                        }
+                    }
+                    if(max < sum){
+                        max = sum;
+                    }
+                }
+            }
+            
+            System.out.println("#" + test_case + " " + max);
+		}
+	}
+}
