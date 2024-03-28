1d201669712ccff76717fbf3600cf0b275f41182
racoi <87438305+racoi@users.noreply.github.com>
2024-03-28 22:49:54 +0900

    [Bronze III] Title: 약수 구하기, Time: 212 ms, Memory: 17772 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        
+        int cnt = 0;
+        for(int i=1; i<=N; i++){
+            if(N % i == 0) cnt++;
+            if(K == cnt){
+                System.out.println(i);
+                break;
+            }
+        }
+        if(K > cnt) System.out.println(0);
+    }
+}
