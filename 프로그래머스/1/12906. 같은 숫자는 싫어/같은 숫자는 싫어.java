c0bf93cbf8cdcb8efce1ff1c431b76c8dbef126a
racoi <87438305+racoi@users.noreply.github.com>
2024-04-19 11:49:28 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 49.71 ms, Memory: 116 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        Stack<Integer> stack = new Stack<Integer>();
+        
+        
+        for(int i=0; i<arr.length; i++){
+            if(!stack.isEmpty()){
+                int temp = stack.peek();
+                if(temp != arr[i]){
+                    stack.push(arr[i]);
+                }
+            } else {
+                stack.push(arr[i]);
+            }
+        }
+        int[] answer = new int[stack.size()];
+        
+        for(int i=stack.size()-1; i>=0; i--){
+            answer[i] = stack.pop();
+        }
+        return answer;
+    }
+}
