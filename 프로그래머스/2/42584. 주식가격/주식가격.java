1f7a568f9f5e27da1a2bcf0835eb608cc7fc3273
racoi <87438305+racoi@users.noreply.github.com>
2024-04-21 21:53:29 +0900

    [level 2] Title: 주식가격, Time: 33.36 ms, Memory: 67.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,16 +1,21 @@
+import java.util.Stack;
 class Solution {
-    public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
-        
-        for(int i=0; i<prices.length; i++){
-            for(int j=i+1; j<prices.length; j++){
-                answer[i]++;
-                if(prices[i] > prices[j]){
-                    break;
-                }
-            }
-        }
-        
-        return answer;
-    }
+	public int[] solution(int[] prices) {
+		int[] answer = new int[prices.length];
+		Stack<Integer> stack = new Stack<>();
+
+		for (int i = 0; i < prices.length; i++) {
+			while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+				answer[stack.peek()] = i - stack.peek();
+				stack.pop();
+			}
+			stack.push(i);
+		}
+
+		while (!stack.isEmpty()) {
+			answer[stack.peek()] = prices.length - stack.peek() - 1;
+			stack.pop();
+		}
+		return answer;
+	}
 }
