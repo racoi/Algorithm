78dda85159f7b60d1b347c4dbd017971c4f1e7b4
racoi <87438305+racoi@users.noreply.github.com>
2024-05-16 12:58:02 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문1, Time: 211 ms, Memory: 34,164 KB -BaekjoonHub

---
+++ SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.util.List;
+import java.util.LinkedList;
+import java.io.FileInputStream;
+
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+
+        for(int test_case = 1; test_case <= 10; test_case++)
+        {
+            int N = sc.nextInt();
+            List<Integer> list = new LinkedList<>();
+            for(int i=0; i<N; i++){
+                list.add(sc.nextInt());
+            }
+
+            int M = sc.nextInt();
+            for(int i=0; i<M; i++){
+                char c = sc.next().charAt(0);
+                int x = sc.nextInt();
+                int y = sc.nextInt();
+                for(int j=x; j<x+y; j++){
+                    list.add(j, sc.nextInt());
+                }
+            }
+
+            System.out.print("#" + test_case);
+            for(int i=0; i<10; i++){
+                System.out.print(" " + list.get(i));
+            }
+            System.out.println();
+        }
+    }
+}
