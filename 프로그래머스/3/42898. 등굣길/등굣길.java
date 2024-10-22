6a1de325d117ffc9b10edd0c8b998039e1c37d47
racoi <87438305+racoi@users.noreply.github.com>
2024-10-22 22:09:40 +0900

    [level 3] Title: 등굣길, Time: 1.00 ms, Memory: 52.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int m, int n, int[][] puddles) {
+        int mod = 1000000007;
+        int[][] dp = new int[n+1][m+1];
+        
+        dp[1][1] = 1;
+        for(int[] puddle : puddles){
+            dp[puddle[1]][puddle[0]] = -1;
+        }
+        
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<m+1; j++){
+                if(dp[i][j] == -1){
+                    continue;
+                }
+                if(dp[i-1][j] > 0){
+                    dp[i][j] = (dp[i][j] + dp[i-1][j]) % mod;
+                }
+                if(dp[i][j-1] > 0){
+                    dp[i][j] = (dp[i][j] + dp[i][j-1]) % mod;
+                }
+            }
+        }
+        
+        return dp[n][m];
+    }
+}
