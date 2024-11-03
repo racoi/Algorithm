941dd564b94bbfb46f2808c88771c07b02f5049d
racoi <87438305+racoi@users.noreply.github.com>
2024-11-03 14:27:33 +0900

    [Silver III] Title: N과 M (1), Time: 1960 ms, Memory: 78604 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static boolean[] visited;
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        arr = new int[M];
+        visited = new boolean[N];
+
+        dfs(0);
+    }
+
+    public static void dfs(int depth) {
+        if(depth == M) {
+            for(int i : arr){
+                System.out.print(i + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for(int i=0; i<N; i++){
+            if(!visited[i]) {
+                visited[i] = true;
+                arr[depth] = i + 1;
+                dfs(depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
