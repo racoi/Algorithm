00ac0e16f256b40d12080ab44e92971ad0f71b64
Yeonju Jung <87438305+racoi@users.noreply.github.com>
2024-12-19 21:29:38 +0900

    [Silver III] Title: N과 M (1), Time: 1736 ms, Memory: 61528 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,10 +1,10 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     static int N, M;
     static int[] arr;
     static boolean[] visited;
-    
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
@@ -12,20 +12,21 @@ public class Main {
 
         arr = new int[M];
         visited = new boolean[N];
-
+        
         dfs(0);
     }
-
-    public static void dfs(int depth) {
-        if(depth == M) {
+    
+    public static void dfs(int depth){
+        if(depth == M){
             for(int i : arr){
                 System.out.print(i + " ");
             }
             System.out.println();
             return;
         }
+        
         for(int i=0; i<N; i++){
-            if(!visited[i]) {
+            if(!visited[i]){
                 visited[i] = true;
                 arr[depth] = i + 1;
                 dfs(depth + 1);
