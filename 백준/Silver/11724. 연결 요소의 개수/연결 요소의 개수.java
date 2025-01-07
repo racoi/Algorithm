83357e290219ca683c6f6f57f6f7c6f29c078ba4
Yeonju Jung <87438305+racoi@users.noreply.github.com>
2025-01-08 00:54:14 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 1444 ms, Memory: 318020 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,41 +1,43 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class Main{
-    static int N, M, count;
+public class Main {
+    static int N;
     static int[][] arr;
     static boolean[] visited;
-
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-
         N = sc.nextInt();
-        M = sc.nextInt();
-
+        int M = sc.nextInt();
         arr = new int[N+1][N+1];
         visited = new boolean[N+1];
 
         for(int i=0; i<M; i++){
-            int num1 = sc.nextInt();
-            int num2 = sc.nextInt();
-
-            arr[num1][num2] = arr[num2][num1] = 1;
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            arr[a][b] = arr[b][a] = 1;
         }
 
-        for(int i=1; i<=N; i++){
+        int cnt = 0;
+        for(int i=1; i<N+1; i++){
             if(!visited[i]){
-                dfs(i);
-                count++;
+                cnt++;
+                bfs(i);
             }
         }
-        System.out.println(count);
+        System.out.println(cnt);
     }
 
-    public static void dfs(int nodeIndex){
-        visited[nodeIndex] = true;
-
-        for(int i=1; i<=N; i++){
-            if(!visited[i] && arr[nodeIndex][i]==1){
-                dfs(i);
+    public static void bfs(int node){
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(node);
+        visited[node] = true;
+        while (!q.isEmpty()){
+            int n = q.poll();
+            for(int i=1; i<N+1; i++){
+                if(arr[n][i] == 1 && !visited[i]){
+                    q.offer(i);
+                    visited[i] = true;
+                }
             }
         }
     }
