d43496e91ec523f9cd2a3e2b2e597eef5fdfd068
Yeonju Jung <87438305+racoi@users.noreply.github.com>
2025-01-11 20:33:31 +0900

    [Silver III] Title: 주유소, Time: 972 ms, Memory: 182876 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -4,15 +4,15 @@ public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int[] arr = new int[N-1];
+        long[] arr = new long[N-1];
         for(int i=0; i<N-1; i++){
-            arr[i] = sc.nextInt();
+            arr[i] = sc.nextLong();
         }
-        
-        int min = Integer.MAX_VALUE;
-        int result = 0;
-        for(int i=0; i<N-1; i++){
-            int price = sc.nextInt();
+
+        long min = sc.nextLong();
+        long result = min * arr[0];
+        for(int i=1; i<N-1; i++){
+            long price = sc.nextLong();
             min = Math.min(min, price);
             result += min * arr[i];
         }
