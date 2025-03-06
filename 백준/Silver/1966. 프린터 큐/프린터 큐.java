a87bc4cd835491c2a604f0f343b7a2f9137f84ff
Yeonju Jung <87438305+racoi@users.noreply.github.com>
2025-03-06 18:25:20 +0900

    [Silver III] Title: 프린터 큐, Time: 264 ms, Memory: 22256 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -1,42 +1,34 @@
-import java.util.Scanner;
-import java.util.LinkedList;
+import java.util.*;
 
-public class Main{
+public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-
         int T = sc.nextInt();
-
-        for(int t=0; t<T; t++){
-            LinkedList<int[]> q = new LinkedList<>();
-            
+        for(int tc=0; tc<T; tc++){
             int N = sc.nextInt();
             int M = sc.nextInt();
 
+            LinkedList<int[]> q = new LinkedList<>();
             for(int i=0; i<N; i++){
                 q.offer(new int[] {i, sc.nextInt()});
             }
 
             int cnt = 0;
             while(!q.isEmpty()){
-                int[] front = q.poll();
+                int[] poll = q.poll();
                 boolean isMax = true;
 
                 for(int i=0; i<q.size(); i++){
-                    if(front[1] < q.get(i)[1]){
-                        q.offer(front);
-                        for(int j = 0; j < i; j++) {
-							q.offer(q.poll());
-						}
+                    if(poll[1] < q.get(i)[1]){
+                        q.offer(poll);
                         isMax = false;
                         break;
                     }
                 }
-
                 if(!isMax) continue;
 
                 cnt++;
-                if(front[0] == M) break;
+                if(poll[0] == M) break;
             }
 
             System.out.println(cnt);
