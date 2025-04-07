cfb4832674a2c142d45292abdb1345f142e7d3ba
Yeonju Jung <87438305+racoi@users.noreply.github.com>
2025-04-07 09:58:51 +0900

    [Silver II] Title: 부당한 퍼즐, Time: 2164 ms, Memory: 326212 KB -BaekjoonHub

---
+++ 백준/Silver/15501. 부당한 퍼즐/부당한 퍼즐.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int[] in = new int[N];
+        for(int i=0; i<N; i++) in[i] = sc.nextInt();
+        int[] out = new int[N];
+        for(int i=0; i<N; i++) out[i] = sc.nextInt();
+
+        for(int i=0; i<N; i++){
+            if(in[0] != out[i]) continue;
+
+            boolean flag = true;
+            for(int j=0; j<N; j++){
+                if(in[j] != out[(i+j)%N]){
+                    flag = false;
+                    break;
+                }
+            }
+
+            boolean rvsflag = true;
+            for(int j=0; j<N; j++){
+                if(in[j] != out[(i+N-j)%N]){
+                    rvsflag = false;
+                    break;
+                }
+            }
+
+            if(flag || rvsflag) System.out.println("good puzzle");
+            else System.out.println("bad puzzle");
+        }
+    }
+}
