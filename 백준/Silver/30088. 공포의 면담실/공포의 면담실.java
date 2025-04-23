cd4d3765ed2efc42b83094bba701120ca03aa14c
Yeonju Jung <87438305+racoi@users.noreply.github.com>
2025-04-23 09:34:06 +0900

    [Silver V] Title: 공포의 면담실, Time: 564 ms, Memory: 85856 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/30088. 공포의 면담실/공포의 면담실.java
+++ 백준/Silver/30088. 공포의 면담실/공포의 면담실.java
@@ -1,22 +1,26 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int arr[] = new int[N];
-        for(int i=0; i<N; i++){
-            int M = sc.nextInt();
-            for(int j=0; j<M; j++){
-                arr[i] += sc.nextInt();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int M = Integer.parseInt(st.nextToken());
+            int sum = 0;
+            for (int j = 0; j < M; j++) {
+                sum += Integer.parseInt(st.nextToken());
             }
+            arr[i] = sum;
         }
 
         Arrays.sort(arr);
-        
         long tmp = 0;
         long ans = 0;
-        for(int i=0; i<N; i++){
+        for (int i = 0; i < N; i++) {
             tmp += arr[i];
             ans += tmp;
         }
