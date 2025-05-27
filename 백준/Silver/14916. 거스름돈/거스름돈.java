26cc8c1514c4189630c39dd193a46af5fd61c571
Yeonju Jung <87438305+racoi@users.noreply.github.com>
2025-05-27 20:19:57 +0900

    [Silver V] Title: 거스름돈, Time: 104 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        
+        while(true){
+            if(N % 5 == 0){
+                cnt += N/5;
+                System.out.println(cnt);
+                break;
+            }else{
+                N -= 2;
+                cnt++;
+            }
+            if(N < 0){
+                System.out.println(-1);
+                break;
+            }
+        }
+    }
+}
