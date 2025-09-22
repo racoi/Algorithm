007cf1ad25f79c3fb4a2c90765d3a9b51c4ee214
Yeonju Jung <87438305+racoi@users.noreply.github.com>
2025-09-22 09:58:48 +0900

    [Silver I] Title: 경로 찾기, Time: 496 ms, Memory: 30808 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[][] arr = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (arr[i][k] == 1 && arr[k][j] == 1) {
+                        arr[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
