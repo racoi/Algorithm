f6b238029ba249b45be1448fb6a53e8837850ec5
Yeonju Jung <87438305+racoi@users.noreply.github.com>
2025-10-28 09:31:25 +0900

    [Silver IV] Title: 등수 구하기, Time: 176 ms, Memory: 17800 KB -BaekjoonHub

---
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    public static void main (String[]args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int new_score = sc.nextInt();
+        int p = sc.nextInt();
+
+        Integer[] arr = new Integer[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        if(n == p && new_score <= arr[arr.length-1])
+            System.out.print(-1);
+        else{
+            int rank = 1;
+            for(int i=0; i<arr.length; i++){
+                if(new_score < arr[i])
+                    rank++;
+                else
+                    break;
+            }
+            System.out.print(rank);
+        }
+    }
+}
